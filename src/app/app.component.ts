17d2844bff3d664ad18d0510a9713f6a8666b72f
Melcn <melaniearinosy@outlook.com>
2023-04-26 09:39:54 +0200

    click function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+
+  count! : number;
+
+  ngOnInit(): void {
+    
+    this.count = 0;
+    
+  }
+
+  clickMoins(){
+    if(this.count > -10){
+      this.count--;
+    }  
+  }
+
+  clickPlus(){
+    if(this.count < 10){
+    this.count++;
+    }
+  }
+
   title = 'ng-click-counter';
 }
